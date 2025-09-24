701e6423a131d16de668ad5e21252b191821e5c3
MatteoCarmignani <matteo.carmignani01@gmail.com>
2025-09-24 16:35:16 +0200

    inserimento heder-main-footer

---
+++ react-dc-comics/src/components/MyFooter.jsx
@@ -0,0 +1,70 @@
+const MyFooter = () => {
+    return (
+    <footer>
+    <div class="top-footer">
+      <div class="container">
+        <div class="column">
+          <h3>DC COMICS</h3>
+          <ul>
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>Movies</li>
+            <li>TV</li>
+            <li>Games</li>
+            <li>Videos</li>
+            <li>News</li>
+          </ul>
+          <h3>SHOP</h3>
+          <ul>
+            <li>Shop DC</li>
+            <li>Shop DC Collectibles</li>
+          </ul>
+        </div>
+
+        <div class="column">
+          <h3>DC</h3>
+          <ul>
+            <li>Terms Of Use</li>
+            <li>Privacy policy (New)</li>
+            <li>Ad Choices</li>
+            <li>Advertising</li>
+            <li>Jobs</li>
+            <li>Subscriptions</li>
+            <li>Talent Workshops</li>
+            <li>CPSC Certificates</li>
+            <li>Ratings</li>
+            <li>Shop Help</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+
+        <div class="column">
+          <h3>SITES</h3>
+          <ul>
+            <li>DC</li>
+            <li>MAD Magazine</li>
+            <li>DC Kids</li>
+            <li>DC Universe</li>
+            <li>DC Power Visa</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="bottom-footer">
+      <div class="container">
+        <button>SIGN-UP NOW!</button>
+        <div class="social">
+          <span>FOLLOW US</span>
+          <img src="img/footer-facebook.png" alt="">
+          <img src="img/footer-twitter.png" alt="">
+          <img src="img/footer-youtube.png" alt="">
+          <img src="img/footer-pinterest.png" alt="">
+          <img src="img/footer-periscope.png" alt="">
+        </div>
+      </div>
+    </div>
+  </footer>
+    )
+}
+
+export default MyFooter
---
+++ react-dc-comics/src/components/MyMain.jsx
@@ -0,0 +1,36 @@
+const MyMain = () => {
+    return (
+        <main>
+            <section class="hero">
+                <p>--&gt; Content goes here &lt;--</p>
+            </section>
+
+            <section class="blue-bar">
+                <div class="container">
+                    <div class="item">
+                        <img src="img/buy-comics-digital-comics.png" alt="">
+                        <span>DIGITAL COMICS</span>
+                    </div>
+                    <div class="item">
+                        <img src="img/buy-comics-merchandise.png" alt="">
+                        <span>DC MERCHANDISE</span>
+                    </div>
+                    <div class="item">
+                        <img src="img/buy-comics-subscriptions.png" alt="">
+                        <span>SUBSCRIPTION</span>
+                    </div>
+                    <div class="item">
+                        <img src="img/buy-comics-shop-locator.png" alt="">
+                        <span>COMIC SHOP LOCATOR</span>
+                    </div>
+                    <div class="item">
+                        <img src="img/buy-dc-power-visa.svg" alt="">
+                        <span>DC POWER VISA</span>
+                    </div>
+                </div>
+            </section>
+        </main>
+    )
+}
+
+export default MyMain
